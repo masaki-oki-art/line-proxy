1c0670547b63228e5473c7147d8f499f3197aa00
masaki-oki-art <masaki.okigami@gmail.com>
2025-10-17 21:32:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import requests
 
 app = Flask(__name__)
 
-
-@app.route("/webhook", methods=["POST"])
+@app.route("/callback", methods=["POST"])
 def webhook():
     data = request.json
     user_message = data["events"][0]["message"]["text"]
 
-#If you send 'ON' via LINE, it will command the Pico W via HTTP.
-    #<pico_IPはローカルIPに書き換え>
-    
+    # <pico_IPはローカルIPに書き換え>
     if user_message.upper() == "ON":
-        requests.post("http://<PICO_IP>/led/on")
+        requests.post("http://192.161.1.16/led/on")
     elif user_message.upper() == "OFF":
-        requests.post("http://<PICO_IP>/led/off")
+        requests.post("http://192.161.1.16/led/off")
 
     return "OK"
 
-#if __name__ == "__main__":   #開発用コードのためコメントアウト
-#    app.run()
+@app.route("/")
+def index():
+    return "Flask app is running."
+
+# if __name__ == "__main__":
+#     app.run()  # 開発用サーバー起動は本番環境では不要
