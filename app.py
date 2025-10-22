f6a4ecc46ffac92fb8f048fbe412cdb5e28877af
masaki-oki-art <masaki.okigami@gmail.com>
2025-10-22 20:53:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import logging
 from flask import Flask, request
 import requests
 
+#PicoW受信成功通知
+def send_line_message(text):
+    payload = {"message": text}
+    headers = {"Content-Type": "application/json"}
+    try:
+        res = requests.post("http://line-proxy-dkvy.onrender.com/notify", json=payload, headers=headers, timeout=10)
+        logging.info("LINE通知ステータス: %s", res.status_code)
+    except Exception as e:
+        logging.error("LINE通知エラー: %s", e)
 # ログ設定（Renderのログ画面に出力される）
 logging.basicConfig(level=logging.INFO)
 
@@ -26,10 +35,11 @@ def callback():
             pico_url = "http://133.207.116.194:7072"  # ← 最新のIPに置き換えてください
             res = requests.post(pico_url, json=data, timeout=30)
             logging.info("Pico response: %s", res.status_code)
+            if res.status_code == 200:
+　　　　　　　　　　　    send_line_message("受信OK")
         else:
             logging.info("非テキストメッセージを受信しました")
     except Exception as e:
         logging.error("Error parsing message: %s", e)
-
 
     return "OK", 200
